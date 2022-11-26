a2f548c3649f56c1adcab4e8e619544e74d75bfb
Carlos <carloshtbraga@gmail.com>
2022-11-25 21:27:01 -0300

    Req6

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -6,8 +6,8 @@ class PlanetCard extends React.Component {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-        <p testid="planet-name">{planetName}</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <p data-testid="planet-name">{planetName}</p>
+        <img alt={ `Planeta ${planetName}` } src={ planetImage } />
       </div>
     );
   }
@@ -16,6 +16,6 @@ class PlanetCard extends React.Component {
 export default PlanetCard;
 
 PlanetCard.propTypes = {
-  planetName: PropTypes.string.isRequired,
   planetImage: PropTypes.string.isRequired,
+  planetName: PropTypes.string.isRequired,
 };
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 import Title from './Title';
 
 class SolarSystem extends React.Component {
@@ -6,6 +8,13 @@ class SolarSystem extends React.Component {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {Planets.map((ele) => (
+          <PlanetCard
+            key={ ele.name }
+            planetImage={ ele.image }
+            planetName={ ele.name }
+          />
+        ))}
       </div>
     );
   }
